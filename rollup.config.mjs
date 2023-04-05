38829a5304aba6c8daa9869052f72ad1cd09cd8b
AmeMeida <ame.pistache@gmail.com>
2023-04-04 23:03:56 -0300

    arquivo de configuração aaaaaaa

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,21 +2,15 @@ import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
 
 export default {
-    input: 'src/index.ts',
-    output: {
-        file: 'dist/index.js',
-        format: 'cjs',
-        sourcemap: true,
-    },
-    plugins: [
-        nodeResolve(),
-        commonjs(),
-        terser(),
-        typescript(),
-    ],
-    external: [
-        'express',
-    ]
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs',
+    sourcemap: true
+  },
+  plugins: [ nodeResolve(), commonjs(), terser(), typescript(), json() ],
+  external: [ 'express' ]
 };
